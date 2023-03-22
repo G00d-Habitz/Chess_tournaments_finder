d94635743fbfec70779ac170158be81c89d01136
Tomasz Chmielewski <123314449+G00d-Habitz@users.noreply.github.com>
2023-03-22 17:38:36 +0100

    Collecting tournament info is now possible
    "tr_list" will be changed in the future as I will be working on a list of "tr" tags (tournaments).

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup as Bs
 import time
 PATH = r"C:\Program Files (x86)\chromedriver.exe"
 
@@ -11,3 +12,21 @@ time.sleep(2)
 print(html)
 with open("html.html", "w",encoding="utf-8") as html_file:
     html_file.write(html)
+    
+
+def get_tournament_info(html):
+    soup = Bs(html, "html.parser")
+    tr_list = soup.find("tr")
+    link_and_name = str(tr_list.find("a")).split('"')
+    place_and_type = tr_list.find_all("div", class_="szary")[1:]
+
+    link = link_and_name[1]
+    tournament_name = link_and_name[4].split("\n")[1].strip()
+    time_control = str(place_and_type[1]).split("\n")[1].strip()
+    place = str(place_and_type[0]).split("\n")[1].strip().split("  ")[0]
+    province = str(tr_list.find("td", {"width": "12%"})).split("\n")[1][-2:]
+    date = str(tr_list.find("td", {"width": "10%"})).split("\n")[1].strip()
+    try:
+        is_fide = str(place_and_type[1]).split("\n")[3].strip()
+    except:
+        is_fide = "bez Fide"
